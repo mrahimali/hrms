14a7ebcdd115624239449e96c98d54820fdf7068
Rahim <mrahimali97@gmail.com>
2026-02-20 15:55:09 +0530

    postgre db

--- backend/hrms/settings.py
+++ backend/hrms/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'hrms.wsgi.application'
 DATABASES = {
 
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
+         "ENGINE": "django.db.backends.postgresql",
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
